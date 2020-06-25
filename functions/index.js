c1658cf550c66ade40a8a6c1f84303a9531650ec
muralimgwl <murali.m@goodworklabs.com>
2020-06-25 18:36:30 +0530

    8_9th_session code

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,34 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mihy-all.firebaseio.com",
+});
+
+var db = admin.database();
+var ref = db.ref("jira-clone");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+
+exports.addProject = functions.https.onRequest((request, response) => {
+  const { body = {} } = request;
+  const { projects = {} } = body;
+  ref
+    .child("projects")
+    .push()
+    .set(projects, (res) => {
+      response.send({ message: "Projects succesfully created" });
+    });
 });
 
-
-exports.addTask = functions.https.onRequest((request, response) => {
-  // console.log("log", request);
-  const {body}=request;
-  console.log(body);
-  //insert to RD
- response.send({"messages":"task added succesfully"});
+exports.getAllProjects = functions.https.onRequest((request, response) => {
+  ref.child("projects").once("value", function (snapshot) {
+    console.log(snapshot.val());
+    response.send({ projects: snapshot.val() });
+  });
 });
